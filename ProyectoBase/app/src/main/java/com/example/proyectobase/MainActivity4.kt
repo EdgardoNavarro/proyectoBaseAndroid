6fa3c4f64be7970f6b064453d170b52330fb4e4b
Edgardo Navarro <edg.navarro@duocuc.cl>
2025-09-12 11:24:52 -0300

    se añadio una nueva ventana con spiner y bton y lista

--- ProyectoBase/app/src/main/java/com/example/proyectobase/MainActivity4.kt
+++ ProyectoBase/app/src/main/java/com/example/proyectobase/MainActivity4.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proyectobase.funciones.OpMatematicas
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +24,10 @@ class MainActivity4 : AppCompatActivity() {
         //val valor1: EditText = findViewById(R.id.et_number1)
         //val valor2: EditText = findViewById(R.id.et_number2)
         //val txResultado: TextView = findViewById(R.id.et_resultado)
-        val btn_calcular : Button= findViewById(R.id.btn_2)
+        val btn_2 : Button= findViewById(R.id.btn_2)
         val spOperaciones:Spinner=findViewById(R.id.sp_mat)
+        val list_ser:ListView=findViewById(R.id.lista_series)
+        val btn_3 : Button= findViewById(R.id.btn_3)
 
         val opcionesSpinner= arrayOf(
             "Sumar"
@@ -32,9 +37,33 @@ class MainActivity4 : AppCompatActivity() {
             , "Dividir"
         )
         //adaptador de array
+
+
         val adaptador = ArrayAdapter( this //contexto
         , android.R.layout.simple_list_item_1 //elementos listview
         , opcionesSpinner )
+
+        spOperaciones.adapter=adaptador
+
+        val opcionesListView = arrayOf(
+            "opcion 1"
+            , "opcion 2"
+            , "opcion 3"
+
+        )
+
+        val adaptador_list = ArrayAdapter(this, android.R.layout.simple_list_item_1
+        , opcionesListView)
+        btn_2.setOnClickListener{
+            var str_op_selected = spOperaciones.selectedItem.toString()
+            val toast = Toast.makeText(this, str_op_selected
+                ,
+                 Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+
+
 
 
 
